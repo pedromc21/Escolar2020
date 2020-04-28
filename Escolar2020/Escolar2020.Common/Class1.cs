69ebffddf0d9eda1b0825def1cb162e00788f459
Ing. Pedro Marquez Cano <pmarquez@gissa.com.mx>
2020-04-28 14:12:39 -0500

    Inicio de proyectos

---
+++ Escolar2020/Escolar2020.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Escolar2020.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Escolar2020/Escolar2020.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Escolar2020.UIClassic
+{
+    public class Class1
+    {
+    }
+}
