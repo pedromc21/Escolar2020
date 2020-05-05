e187b990add94bfaecda92063ed67271c56fa50e
Ing. Pedro Marquez Cano <pmarquez@gissa.com.mx>
2020-05-05 09:19:40 -0500

    Api one

---
+++ Escolar2020/Escolar2020.Web/Controllers/API/App_TutorController.cs
@@ -0,0 +1,24 @@
+﻿namespace Escolar2020.Web.Controllers.API
+{
+    using Escolar2020.Web.Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+    public class App_TutorController : Controller
+    {
+        private readonly ITutorRepository tutorRepository;
+
+        public App_TutorController(ITutorRepository tutorRepository)
+        {
+            this.tutorRepository = tutorRepository;
+        }
+        
+        [HttpGet]
+        public IActionResult GetTutors()
+        {
+            //Regresa todos los datos del controlador Tutor, tengo que hacer que salga solo los tutores de la familia.
+            return Ok(this.tutorRepository.GetAll());
+        }
+
+    }
+}
