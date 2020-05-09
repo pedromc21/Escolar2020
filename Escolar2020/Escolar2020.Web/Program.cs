25f38bc6505c9f0fbd9c0ceada73f80252b3fb1f
Ing. Pedro Marquez Cano <pmarquez@gissa.com.mx>
2020-05-09 10:05:13 -0500

    Publicacion 01

--- Escolar2020/Escolar2020.Web/Program.cs
+++ Escolar2020/Escolar2020.Web/Program.cs
@@ -4,6 +4,7 @@
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
 
     public class Program
     {
